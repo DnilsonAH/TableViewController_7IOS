7427b90c1a6f5992fa68ab7f96e4eacf088bc114
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2025-06-11 16:57:42 -0500

    Avance hasta punto 24

---
+++ TableViewController/TableViewControllerNuevo.swift
@@ -0,0 +1,106 @@
+//
+//  TableViewControllerNuevo.swift
+//  TableViewController
+//
+//  Created by Dnilson Achahuanco on 11/06/25.
+//
+
+import UIKit
+
+class TableViewControllerNuevo: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    var arregloNumeros:[String] = ["1", "2", "3", "4"]
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return arregloNumeros.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = arregloNumeros[indexPath.row]
+        switch cell.textLabel?.text {
+        case "1":
+            cell.imageView?.image = UIImage(named: "ICONOS/1.png")
+            cell.detailTextLabel?.text = "Celda numero 1"
+        case "2":
+            cell.imageView?.image = UIImage(named: "ICONOS/2.png")
+            cell.detailTextLabel?.text = "Celda numero 2"
+        case "3":
+            cell.imageView?.image = UIImage(named: "ICONOS/3.png")
+            cell.detailTextLabel?.text = "Celda numero 3"
+        case "4":
+            cell.imageView?.image = UIImage(named: "ICONOS/4.png")
+            cell.detailTextLabel?.text = "Celda numero 4"
+        default:
+            print("No hay mas elementos para llenar imagen")
+            cell.detailTextLabel?.text = "Celda fuera de rango"
+        }
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            //tableView.deleteRows(at: [indexPath], with: .fade)
+            arregloNumeros.remove(at: indexPath.row)
+            tableView.reloadData()
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the
+            // array, and add a new row to the table view
+        }
+    }
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
